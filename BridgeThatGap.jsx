de63adae5ed2a24b9158928121e0129b9a740cb2
glindas-sparkle <ariel@yourbrandamplified.com>
2026-07-10 22:37:41 -0500

    Create BridgeThatGap.jsx
    adding new section for photo

---
+++ BridgeThatGap.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function BridgeThatGap() {
+  return (
+    <div id="bridge-that-gap">
+      <img
+        src="/bridge-that-gap.png"
+        alt="Bridging the Gap in Cardiac Care"
+        className="bridge-img"
+      />
+    </div>
+  )
+}
